96b9f862e595856f1b11ffd2be186a651355c6d3
James <j.r.precilla365@gmail.com>
2025-02-19 10:30:19 +0100

    added some more improvements to the video tag in the js

--- portFolio.js
+++ portFolio.js
@@ -1,4 +1,15 @@
 "use strict";
-document.addEventListener("click", () => {
-    console.log("hello");
+const videoTag = document.querySelector("video");
+const toggleVideo = document.getElementById("videoCheckBox");
+console.log(toggleVideo);
+toggleVideo === null || toggleVideo === void 0 ? void 0 : toggleVideo.addEventListener("change", function () {
+    if (!toggleVideo.checked) {
+        videoTag === null || videoTag === void 0 ? void 0 : videoTag.pause();
+    }
+    else {
+        videoTag === null || videoTag === void 0 ? void 0 : videoTag.play();
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    videoTag.pause();
 });
--- portFolio.ts
+++ portFolio.ts
@@ -1,3 +1,17 @@
-document.addEventListener("click", () => {
-    console.log("hello")
+
+const videoTag = document.querySelector("video") as HTMLVideoElement
+const toggleVideo = document.getElementById("videoCheckBox") as HTMLInputElement
+
+console.log(toggleVideo)
+
+toggleVideo?.addEventListener("change", function () {
+    if (!toggleVideo.checked) {
+        videoTag?.pause()
+    } else {
+        videoTag?.play()
+    }
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    videoTag.pause()
 })
